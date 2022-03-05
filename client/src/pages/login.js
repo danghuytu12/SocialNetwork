94f13a0fa4f6b4779f79c1faa91e3ad89ca2afe0
danghuytu12 <86486246+danghuytu12@users.noreply.github.com>
2022-03-05 08:59:11 +0700

    fix title

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -60,7 +60,7 @@ const Login = () => {
     return (
         <div className="auth_page">
             <form onSubmit={handleSubmit}>
-                <h3 className="text-uppercase text-center mb-4">TU-Network</h3>
+                <h3 className="text-uppercase text-center mb-4">Instagram</h3>
 
                 <div className="form-group">
                     {/* <label htmlFor="exampleInputEmail1">Email address</label> */}
--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -71,7 +71,7 @@ const Register = () => {
     return (
         <div className="auth_page">
             <form onSubmit={handleSubmit}>
-                <h3 className="text-uppercase text-center mb-4">TU-Network</h3>
+                <h3 className="text-uppercase text-center mb-4">Instagram</h3>
 
                 <div className="form-group">
                     {/* <label htmlFor="fullname">Full Name</label> */}
